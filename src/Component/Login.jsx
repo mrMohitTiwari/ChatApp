6daae8dd6deec9af13a803b52fd8821eb2b1709e
mrMohitTiwari <mt0988145@gmail.com>
2024-11-14 23:23:46 +0530

    updating login form UI

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -5,7 +5,13 @@ export default function App() {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="m-auto mt-[70px] flex h-[350px] w-[350px] flex-col gap-y-1 border-4 p-10 shadow-xl shadow-zinc-300"
+    >
+      <h2 className="mb-2 text-center font-mono text-2xl font-bold">
+        Login Form
+      </h2>
       <input
         placeholder="USERNAME"
         {...register('username', { required: true, maxLength: 20 })}
